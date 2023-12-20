8880aa50780ac6129a404e3756d7ae6ca0a28883
Chaftar-Yosser <ch.yosser1999@gmail.com>
2023-12-21 00:59:51 +0100

    Signing Up & Hashing Passwords , Email & Password Validation

--- models/userModel.js
+++ models/userModel.js
@@ -25,35 +25,31 @@ const userSchema = new Schema({
 })
 
 // static signup method
-userSchema.statics.signup = async function(email, password) {
-
+userSchema.statics.signup = async function(email, password, name) {
     // validation à l'aide de validator package
-    if (!email || !password) {
-      throw Error('All fields must be filled')
+    if (!name || !email || !password) {
+        throw Error('All fields must be filled');
     }
     if (!validator.isEmail(email)) {
-      throw Error('Email not valid')
+        throw Error('Email not valid');
     }
     if (!validator.isStrongPassword(password)) {
-      throw Error('Password not strong enough')
+        throw Error('Password not strong enough');
     }
 
-
-    // vrérification if email exist ou non  
-    const exists = await this.findOne({ email })
-
+    // vrérification if email exist ou non 
+    const exists = await this.findOne({ email });
     if (exists) {
-        throw Error('Email already in use')
+        throw Error('Email already in use');
     }
 
-    const salt = await bcrypt.genSalt(10)
-    
-    const hash = await bcrypt.hash(password, salt)
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
 
     // pour stocker dans la BD un user avec mdp et email
-    const user = await this.create({ email, password: hash })
-    return user
+    const user = await this.create({ name, email, password: hash });
+    return user;
+};
 
-}
 
 module.exports = mongoose.model('User', userSchema)
