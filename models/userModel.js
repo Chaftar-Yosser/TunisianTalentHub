2385a53b1d08e885e8ca76dde4a13c1de751d07a
Chaftar-Yosser <ch.yosser1999@gmail.com>
2023-12-21 02:19:53 +0100

    Signing&Logging in with Token

--- models/userModel.js
+++ models/userModel.js
@@ -25,12 +25,13 @@ const userSchema = new Schema({
 })
 
 // static signup method
-userSchema.statics.signup = async function(email, password, name) {
+userSchema.statics.signup = async function(name, email, password) {
     // validation à l'aide de validator package
     if (!name || !email || !password) {
         throw Error('All fields must be filled');
     }
     if (!validator.isEmail(email)) {
+        console.log('Invalid email:', email);
         throw Error('Email not valid');
     }
     if (!validator.isStrongPassword(password)) {
@@ -50,6 +51,27 @@ userSchema.statics.signup = async function(email, password, name) {
     const user = await this.create({ name, email, password: hash });
     return user;
 };
+
+
+// static login method
+userSchema.statics.login = async function(email, password) {
+
+    if (!email || !password) {
+      throw Error('All fields must be filled')
+    }
+  
+    const user = await this.findOne({ email })
+    if (!user) {
+      throw Error('Incorrect email')
+    }
+  
+    const match = await bcrypt.compare(password, user.password)
+    if (!match) {
+      throw Error('Incorrect password')
+    }
+  
+    return user
+}
 
 
 module.exports = mongoose.model('User', userSchema)
